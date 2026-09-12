c31d8c2c46cf74b784e549ab7403984a84232d5d
shivaji <shivakumargunti123@gmail.com>
2026-09-12 16:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	if(!str||str.length===0){
+		return null;
+	}
+	let map=new Map();
+	for(let i=0;i<str.length();i++){
+		if(map.has(i)){
+			map.set(i,map.get(i)+1);
+		}
+		else{
+			map.set(i,1);
+		}
+	}
+	for(let i=0;i<str.length;i++){
+		if(map.get(i)===1){
+			return i;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
